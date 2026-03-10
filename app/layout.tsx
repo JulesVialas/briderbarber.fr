c1ad13637ef9f681bac9d69fe89a06f2b26a2a3e
Jules Vialas <jules.vialas@gmail.com>
2026-03-10 19:06:54 +0100

    perf: add preconnect hints and image optimizations

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr">
+      <head>
+        <link rel="dns-prefetch" href="https://www.instagram.com" />
+        <link rel="dns-prefetch" href="https://calendly.com" />
+        <link rel="dns-prefetch" href="https://maps.google.com" />
+        <link rel="preconnect" href="https://www.instagram.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://calendly.com" crossOrigin="anonymous" />
+      </head>
       <body className={`${spaceGrotesk.variable} antialiased`}>
         {children}
       </body>
